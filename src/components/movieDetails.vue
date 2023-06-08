e2c87d029f08cb63e8278a98d7097ee498d92609
MirkoGLostia <giggi.torre@mail.com>
2023-06-08 22:39:47 +0200

    mls2 gestione bandiere

--- src/components/movieDetails.vue
+++ src/components/movieDetails.vue
@@ -10,20 +10,25 @@ export default {
 <template>
     <section>
         <div>
+            <!-- title search -->
             <span>
                 {{ details.title }}
                 {{ details.name }}
             </span>
             <br>
+            <!-- original title -->
             <span>
                 {{ details.original_title }}
                 {{ details.original_name }}
             </span>
             <br>
+            <!-- language -->
             <span>
                 {{ details.original_language }}
+                <img :src="`/img/${details.original_language}.png`" alt="flag">
             </span>
             <br>
+            <!-- vote -->
             <span>
                 {{ details.vote_average }}
             </span>
