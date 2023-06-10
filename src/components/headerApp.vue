d54432b808d248caf370c8ee3bb9ea15eec71f8c
MirkoGLostia <giggi.torre@mail.com>
2023-06-10 16:24:17 +0200

    header style

--- src/components/headerApp.vue
+++ src/components/headerApp.vue
@@ -12,17 +12,45 @@ export default {
 </script>
 
 <template>
-    <section>
-        <form>
-            <input type="text" placeholder="search film" id="searchBarFilm" v-model="store.searchText">
-            <button type="submit" @click.prevent="$emit('startSearch')">
-                search
-            </button>
-        </form>
-    </section>
+    <header>
+        <!-- logo -->
+        <div>
+            <h1>
+                BOOLFLIX
+            </h1>
+        </div>
+
+        <!-- search bar -->
+        <div>
+            <form>
+                <input type="text" placeholder="search film" id="searchBarFilm" v-model="store.searchText"
+                    @keypress.enter.prevent="$emit('startSearch')">
+            </form>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
 @use './../styles/partials/variables' as v;
 @use './../styles/partials/mixins' as *;
+
+header {
+    background-color: black;
+    height: 70px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+
+    h1 {
+        color: v.$myred;
+    }
+
+    input {
+        width: 300px;
+        padding: 2px;
+        font-size: 18px;
+    }
+}
 </style>
