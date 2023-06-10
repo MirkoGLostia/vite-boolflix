1151a68153ebf4d52dfda52277a441f1a583fb67
MirkoGLostia <giggi.torre@mail.com>
2023-06-10 16:24:37 +0200

    main container style

--- src/components/mainFilmShow.vue
+++ src/components/mainFilmShow.vue
@@ -18,22 +18,48 @@ export default {
 </script>
 
 <template>
-    <section>
+    <section id="main-container-sections">
         <h2>MOVIES</h2>
-        <div v-for="video in store.movieData" :key="video.id">
-            <posterImage :details="video" />
-            <movieDetails :details="video" />
-        </div>
+        <section>
+            <div v-for="video in store.movieData" :key="video.id">
+                <posterImage :details="video" />
+                <movieDetails :details="video" />
+            </div>
+        </section>
 
         <h2>TV SERIES</h2>
-        <div v-for="video in store.seriesData" :key="video.id">
-            <posterImage :details="video" />
-            <movieDetails :details="video" />
-        </div>
+        <section>
+            <div v-for="video in store.seriesData" :key="video.id">
+                <posterImage :details="video" />
+                <movieDetails :details="video" />
+            </div>
+        </section>
     </section>
 </template>
 
 <style lang="scss" scoped>
 @use './../styles/partials/variables' as v;
 @use './../styles/partials/mixins' as *;
+
+#main-container-sections {
+    background-color: v.$myblack;
+    height: calc(100vh - 70px);
+    overflow: hidden;
+    overflow-y: auto;
+
+    h2 {
+        padding: 20px;
+    }
+
+    section {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0 auto;
+
+        div {
+            width: 340px;
+            margin: 50px;
+        }
+    }
+}
 </style>
